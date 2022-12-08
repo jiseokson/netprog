0df863399f81fd2c149a3117a8f3a5942a304ad0
Jiseok Son <jiseokson@gmail.com>
2022-12-08 17:36:30 +0900

    calc server and client demo
    operator(1 byte), operand count(1 byte, signed char), integers(4 byte, signed int)

---
+++ 05/calc/calc-client.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUFFER_SIZE 1024
+
+void error_handling(const char *message)
+{
+    fprintf(stderr, "%s\n", message);
+    exit(1);
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 3) {
+        printf("Usage: %s <IP address> <port>\n", av[0]);
+        exit(1);
+    }
+
+    int client_socket = socket(PF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_address;
+
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = PF_INET;
+    server_address.sin_addr.s_addr = inet_addr(av[1]);
+    server_address.sin_port = htons(atoi(av[2]));
+
+    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+        error_handling("connect() error");
+    else
+        printf("Connection success!\n");
+
+    char operartor;
+    char operand_cnt;
+    printf("operator: ");
+    scanf("%c", &operartor);
+    printf("operand cnt: ");
+    scanf("%hhd", &operand_cnt);
+    write(client_socket, &operartor, 1);
+    write(client_socket, &operand_cnt, 1);
+
+    for (int i = 0; i < operand_cnt; ++i) {
+        int n;
+        printf("num %d: ", i);
+        scanf("%d", &n);
+        write(client_socket, &n, 4);
+    }
+
+    close(client_socket);
+    return 0;
+}
---
+++ 05/calc/calc-server.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUFFER_SIZE 1024
+
+void error_handling(const char *message)
+{
+    fprintf(stderr, "%s\n", message);
+    exit(1);
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2) {
+        printf("Usage: %s <port>\n", av[0]);
+        exit(1);
+    }
+
+    int server_socket;
+    struct sockaddr_in server_address;
+
+    server_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1)
+        error_handling("socket() error");
+
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(atoi(av[1]));
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+        error_handling("bind() error");
+
+    if (listen(server_socket, 5) == -1)
+        error_handling("listen() error");
+
+    struct sockaddr_in client_address;
+    socklen_t client_address_size = sizeof(client_address);
+
+    int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_size);
+    if (client_socket == -1)
+        error_handling("accpet() error");
+    else
+        printf("Connection success!\n");
+
+    ssize_t received = 0;
+    ssize_t readed = 0;
+
+    char operator;
+    char operand_cnt = 0;
+    char buffer[BUFFER_SIZE];
+
+    while (received < 1) {
+        readed = read(client_socket, &operator, 1);
+        if (readed == -1)
+            error_handling("read() error: get operator");
+        received += readed;
+    }
+    while (received < 2) {
+        readed = read(client_socket, &operand_cnt, 1);
+        if (readed == -1)
+            error_handling("read() error: get operand cnt");
+        received += readed;
+    }
+
+    received = 0;
+    while (received < operand_cnt * sizeof(int)) {
+        readed = read(client_socket, buffer + received, BUFFER_SIZE - received);
+        if (readed == -1)
+            error_handling("read() error: get operands");
+        received += readed;
+    }
+
+    // for test
+    printf("operator: %c, operand cnt: %d\n", operator, operand_cnt);
+    printf("num stream\n");
+    for (int i = 0; i < received; ++i)
+        printf("%.2x ", buffer[i]);
+    printf("\n");
+
+    close(client_socket);
+    close(server_socket);
+    return 0;
+}
